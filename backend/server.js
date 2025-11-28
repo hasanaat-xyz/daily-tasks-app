4349244e6078e7f3e7af9e45cf84e98dbfd91b0f
hasanaat <arsheema619@gmail.com>
2025-11-28 14:13:34 +0530

    daily-tasks-app

--- backend/server.js
+++ backend/server.js
@@ -8,9 +8,9 @@ mongoose
     // generate today's tasks
     await generateTasksForToday();
     console.log("✅ Today's tasks generated");
-
     app.listen(PORT, () => {
       console.log(`🚀 Server running on http://localhost:${PORT}`);
     });
   })
+
   .catch((err) => console.error("❌ MongoDB error:", err.message));
