810dc468859ac998c25704e757803b7db7553ec6
hasanaat <arsheema619@gmail.com>
2025-11-29 00:23:55 +0530

    create task

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -29,10 +29,10 @@ router.post("/", async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 });
-
 // TOGGLE complete
 router.patch("/:id", async (req, res) => {
   try {
+    
     const task = await Task.findById(req.params.id);
     if (!task) return res.status(404).json({ message: "Task not found" });
     task.completed = !task.completed;
