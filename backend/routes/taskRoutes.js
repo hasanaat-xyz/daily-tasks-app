1e7fab6baf8d4d6acc9d62582b07c6f1912e8492
hasanaat <arsheema619@gmail.com>
2025-11-28 21:42:29 +0530

    create tasl

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -5,9 +5,7 @@ const router = express.Router();
 
 // GET tasks by username + date
 router.get("/:userName/:date", async (req, res) => {
-  try {
-    
-    const { userName, date } = req.params;
+  try {    const { userName, date } = req.params;
     const tasks = await Task.find({ userName, date });
     res.json(tasks);
   } catch (error) {
