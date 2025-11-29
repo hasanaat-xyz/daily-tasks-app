c97771c0c3716d834eedc494a148d76fa0e8b900
hasanaat <arsheema619@gmail.com>
2025-11-30 03:21:06 +0530

    components

--- frontend/src/components/TaskCard.jsx
+++ frontend/src/components/TaskCard.jsx
@@ -19,6 +19,7 @@ export default function TaskCard({ task, refresh }) {
         >
           {task.title}
         </h2>
+        
         {task.missed && (
           <p className="text-red-500 text-sm">Missed</p>
         )}
