2017f72305b9891d507e9203739732fb52cc43f9
hasanaat <arsheema619@gmail.com>
2025-12-01 12:11:59 +0530

    server.js/catch-error

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(async () => {
     console.log("✅ MongoDB connected");
+    
     // generate today's tasks
     await generateTasksForToday();
     console.log("✅ Today's tasks generated");
@@ -11,4 +12,5 @@ mongoose
       console.log(`🚀 Server running on http://localhost:${PORT}`);
     });
   })
+
   .catch((err) => console.error("❌ MongoDB error:", err.message));
