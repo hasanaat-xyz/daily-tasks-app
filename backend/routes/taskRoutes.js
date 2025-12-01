e8069ccffdf8aa001fa18eaee9fb549713aa3e3c
hasanaat <arsheema619@gmail.com>
2025-12-01 12:17:44 +0530

    createTasks

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -12,6 +12,7 @@ router.get("/:userName/:date", async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 });
+
 // CREATE task
 router.post("/", async (req, res) => {
   try {
