a2b69e6ac38b1d03bc9f3cd1c1a8981b804750e7
hasanaat <arsheema619@gmail.com>
2025-12-06 13:30:11 +0530

    updatedTASKS

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -28,7 +28,6 @@ export const createTask = async (req, res) => {
 
 export const getAllTasksByUser = async (req, res) => {
   try {
-    
     const { userId } = req.params;
     const tasks = await Task.find({ userId }).sort({ createdAt: -1 });
     res.json(tasks);
