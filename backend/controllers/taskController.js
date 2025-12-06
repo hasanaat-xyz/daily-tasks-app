dca4514ef1ec5bbb3dce04c43361b9c31fb1048a
hasanaat <arsheema619@gmail.com>
2025-12-06 13:27:16 +0530

    updatedTASKS

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -58,6 +58,7 @@ export const updateTaskStatus = async (req, res) => {
       message: "✅ Task updated",
       task: updatedTask,
     });
+    
   } catch (error) {
     res.status(500).json({
       message: "❌ Error updating task",
