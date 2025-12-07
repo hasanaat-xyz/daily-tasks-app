fe445b42eb284d1a270a8f0463c41cd76b50ec08
hasanaat <arsheema619@gmail.com>
2025-12-07 23:19:02 +0530

    server.js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,16 +3,16 @@ import Dashboard from "./pages/Dashboard";
 import Login from "./pages/LoginPage";
 
 export default function App() {
-  return (
+  return ( 
     <Router>
       <div className="min-h-screen bg-[#f6f7fb] text-gray-800">
         <Routes>
           {/* Login Page */}
           <Route path="/" element={<Login />} />
           {/* Main Dashboard */}
-          
           <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
+        
       </div>
     </Router>
   );
