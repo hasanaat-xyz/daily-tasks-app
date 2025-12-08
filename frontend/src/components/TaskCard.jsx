05e1753db1c26025cde2c085a2cd7181492c5750
hasanaat <arsheema619@gmail.com>
2025-12-08 11:03:55 +0530

    /taskcard improvised

--- frontend/src/components/TaskCard.jsx
+++ frontend/src/components/TaskCard.jsx
@@ -9,8 +9,6 @@ export default function TaskCard({ task, refresh }) {
       console.log(err);
     }
   };
-
-  
   return (
     <div className="p-4 mb-3 border rounded-lg flex justify-between items-center">
       <div>
