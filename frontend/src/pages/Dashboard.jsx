271eb9beca360bc7211daba2d7ad871204efe75c
hasanaat <arsheema619@gmail.com>
2025-12-09 12:02:56 +0530

    dashboard updated

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -19,9 +19,10 @@ export default function Dashboard() {
 
   // Fetch tasks for ONE user
   const getTasksForUser = useCallback(async (userName) => {
-    
+
     try {
       const res = await axios.get(
+        
         `http://localhost:5000/api/tasks/${userName}/${today}`
       );
       return res.data;
