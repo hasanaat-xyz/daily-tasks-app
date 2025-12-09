5665d779c76c4e03bad0772cc26e847f3511ff50
hasanaat <arsheema619@gmail.com>
2025-12-09 12:02:41 +0530

    dashboard updated

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -16,6 +16,7 @@ export default function Dashboard() {
     humera: [],
     hafsah: []
   });
+  
   // Fetch tasks for ONE user
   const getTasksForUser = useCallback(async (userName) => {
     try {
