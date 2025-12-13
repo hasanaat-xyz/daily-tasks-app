3e56c6692e2bbff86e263ca91de2ffa560b72cc5
hasanaat <arsheema619@gmail.com>
2025-12-13 15:37:32 +0530

    model/task.js

--- backend/models/Task.js
+++ backend/models/Task.js
@@ -1,28 +1,31 @@
 import mongoose from "mongoose";
 
-const taskSchema = new mongoose.Schema({
-  userName: {
-    type: String,
-    required: true,
-    enum: ["hasanaat", "humera", "hafsah"]
+const taskSchema = new mongoose.Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+      enum: ["hasanaat", "humera", "hafsah"],
+    },
+
+    title: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: String, // "YYYY-MM-DD"
+      required: true,
+    },
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+    missed: {
+      type: Boolean,
+      default: false,
+    },
   },
-  
-  title: {
-    type: String,
-    required: true
-  },
-  date: {
-    type: String, // "YYYY-MM-DD"
-    required: true
-  },
-  completed: {
-    type: Boolean,
-    default: false
-  },
-  missed: {
-    type: Boolean,
-    default: false
-  }
-}, { timestamps: true });
+  { timestamps: true }
+);
 
 export default mongoose.model("Task", taskSchema);
