9a6643b28201aa884c45d975a1117880cb618629
hasanaat <arsheema619@gmail.com>
2025-12-23 17:25:04 +0530

    generate today's task function added

--- backend/server.js
+++ backend/server.js
@@ -7,10 +7,11 @@ mongoose
     // generate today's tasks
 
     await generateTasksForToday();
+    
     console.log("✅ Today's tasks generated");
     app.listen(PORT, () => {
       console.log(`🚀 Server running on http://localhost:${PORT}`);
     });
   })
-  
+
   .catch((err) => console.error("❌ MongoDB error:", err.message));
