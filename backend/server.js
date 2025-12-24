50bb24288c1e2d01fd3cb5bd61027eb27e70c61b
hasanaat <arsheema619@gmail.com>
2025-12-24 18:09:38 +0530

    server.js

--- backend/server.js
+++ backend/server.js
@@ -11,5 +11,5 @@ mongoose
     app.listen(PORT, () => {
       console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
-  })
+  }) 
   .catch((err) => console.error("❌ MongoDB error:", err.message));
