6ea598f386353d665125497d019559e19272aa5f
hasanaat <arsheema619@gmail.com>
2025-12-29 15:19:15 +0530

    loadAllTasks function added

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -44,7 +44,6 @@ export default function Dashboard() {
   useEffect(() => {
 
     loadAllTasks();
-    
   }, [loadAllTasks]);
   return (
     <div className="min-h-screen bg-linear-to-br from-indigo-50 via-purple-50 to-pink-50 p-8">
