1484792c8bef4cffa511179851d1b500e639bbde
hasanaat <arsheema619@gmail.com>
2025-12-31 18:34:40 +0530

    dashboard updated

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -76,6 +76,7 @@ export default function Dashboard() {
                     refresh={loadAllTasks}
                   />
                 ))}
+                
               </div>
             )}
           </div>
