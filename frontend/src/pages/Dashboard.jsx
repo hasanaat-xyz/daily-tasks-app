9705cb09f625f3b0005d455f8bb1a3e504f93941
hasanaat <arsheema619@gmail.com>
2026-01-02 09:16:59 +0530

    fetch tasks for one user

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -22,7 +22,6 @@ export default function Dashboard() {
       const res = await axios.get(
         `http://localhost:5000/api/tasks/${userName}/${today}`
       );
-      
       return res.data;
     } catch (error) {
       console.error("Error fetching tasks for", userName, error);
