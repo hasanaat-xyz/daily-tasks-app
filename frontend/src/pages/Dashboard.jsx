b6318211825f093ef8889d0cde49ccc6d4aa682f
hasanaat <arsheema619@gmail.com>
2026-01-02 09:28:59 +0530

    loadAlltasks

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -42,7 +42,6 @@ export default function Dashboard() {
     setAllTasks({ hasanaat, humera, hafsah });
   }, [getTasksForUser]);
   useEffect(() => {
-
     loadAllTasks();
   }, [loadAllTasks]);
   return (
