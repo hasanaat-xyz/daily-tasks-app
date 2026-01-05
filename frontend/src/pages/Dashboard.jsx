a0701d74fd796cfb3f9f0b95c62b57a668de2637
hasanaat <arsheema619@gmail.com>
2026-01-05 13:52:51 +0530

    loadAlltasksd

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -67,7 +67,6 @@ export default function Dashboard() {
               </p>
             ) : (
               <div className="space-y-4">
-
                 {allTasks[person.id].map((task) => (
                   <TaskCard
                     key={task._id}
