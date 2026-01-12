cbdeeb8ff0a49f7622acef895d5c5ed7dbbebfb6
hasanaat <arsheema619@gmail.com>
2026-01-12 07:28:25 +0530

    loadAlltasksd

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -74,7 +74,6 @@ export default function Dashboard() {
                     task={task}
                     refresh={loadAllTasks}
                   />
-                  
                 ))}
               </div> 
             )}
